6c456fe916893482026fd40840d91a1d16034c67
clayton <me@clayton.cc>
2025-12-04 12:38:39 -0800

    Assignment 2

--- my_epic_module.v
+++ my_epic_module.v
@@ -1,3 +1,9 @@
-module top (output [8:0] out, input [7:0] in);
-	assign out = in << 1;
+module top (
+	output reg [8:0] out,
+	input [7:0] in,
+	input clock
+);
+	always @(posedge clock) begin
+		assign out = in << 1;
+	end
 endmodule
--- my_epic_module_test.v
+++ my_epic_module_test.v
@@ -1,12 +1,21 @@
 module grade_my_epic_module_cuz_im_a_ta_now_just_like_francisco_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay_yay;
 	reg [7:0] in;
+	reg clock;
 	wire [8:0] out;
 
-	top subject (.out (out), .in (in));
+	top subject (.out (out), .in (in), .clock (clock));
 
 	initial begin
-		in = 8'b00000000; #10; // i think the #10 waits or something
+		clock = 0;
+		forever #10 clock = !clock; // pos edge on odd * 10
+	end
+
+	initial begin
+		#5;
+		in = 8'b00000000; #10;
 		in = 8'b00000001; #10;
+		in = 8'b00000010; #10;
+		in = 8'b00000100; #10;
 		in = 8'b10000000; #10;
 		in = 8'b11111111; #10;
 
