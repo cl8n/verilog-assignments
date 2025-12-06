ab97262f843249a7ac23b1d087b0e86950bd8268
clayton <me@clayton.cc>
2025-12-06 07:09:16 -0800

    Wait for clock in expect task

--- 03/test.v
+++ 03/test.v
@@ -7,42 +7,43 @@ module test;
 	clock_generator m1 (clock);
 	assignment3 m2 (out, in, op, clock);
 
-	task expect (input integer expected_out);
-		if (out == expected_out)
-			$display("PASS: %d", expected_out);
-		else
-			$display("FAIL: Expected %d, Actual %d", expected_out, out);
+	task expect_after_clock (input [8:0] expected_out);
+		begin
+			@(posedge clock); #1;
+
+			if (out == expected_out)
+				$display("PASS: %d", expected_out);
+			else
+				$display("FAIL: Expected %d, Actual %d", expected_out, out);
+		end
 	endtask
 
 	initial begin
-		#5;
-
-		in = 0; op = 0; #10;
-		/* expect out to be unknown here */ #10;
+		in = 0; op = 0;
+		@(posedge clock); #1;
+		// expect_after_clock(9'bX);
 
 		// in is buffered for one clock cycle
-		in = 9; #10;
-		expect(0); #10;
+		in = 9;
+		expect_after_clock(0);
 
 		// ...so only at this point we'll see the updated value
-		#10;
-		expect(9); #10;
+		expect_after_clock(9);
 
 		// but op is not buffered, so it will affect out on the next clock cycle
-		op = 1; #10;
-		expect(11); #10;
+		op = 1;
+		expect_after_clock(11);
 
-		op = 2; #10;
-		expect(18); #10;
+		op = 2;
+		expect_after_clock(18);
 
-		op = 3; #10;
-		expect(2); #10;
+		op = 3;
+		expect_after_clock(2);
 
-		in = 128 + 15; #10;
-		expect(2); #10;
+		in = 128 + 15;
+		expect_after_clock(2);
 
-		#10;
-		expect(5); #10;
+		expect_after_clock(5);
 
 		$finish;
 	end
