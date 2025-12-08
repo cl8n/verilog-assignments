271b339f58d7b88f4b738773575067cac195a268
clayton <me@clayton.cc>
2025-12-08 14:43:20 -0800

    Rewrite assignment 6 to separate combinatorial logic from synchronous logic

--- 06/assignment6.v
+++ 06/assignment6.v
@@ -5,57 +5,31 @@ module assignment6_part1 (
 	input wire clk,
 	input wire rst
 );
-	localparam L = 0;
-	localparam R = 1;
+	wire parsed_turn;
+	wire signed [15:0] amount;
+	parser m_parser (parsed_turn, amount, character, enable_character, clk, rst);
 
 	reg signed [15:0] dial;
 
-	reg direction;
-	reg signed [15:0] amount;
+	reg signed [15:0] s_dial_signed;
+	reg [15:0] s_dial;
+	reg [15:0] s_result;
+
+	always @* begin
+		s_dial_signed = (dial + amount) % 100;
+		s_dial = s_dial_signed < 0 ? s_dial_signed + 100 : s_dial_signed;
+		s_result = s_dial == 0 ? result + 1 : result;
+	end
 
 	always @(posedge clk) begin
 		if (rst) begin
-			result <= 0;
 			dial <= 50;
-			reset_parser();
-		end else if (enable_character)
-			case (character)
-				"L": direction <= L;
-				"R": direction <= R;
-				"\n":
-					begin
-						turn_dial();
-						reset_parser();
-					end
-				default:
-					if (character >= "0" && character <= "9")
-						amount <= amount * 10 + character - "0";
-			endcase
+			result <= 0;
+		end else if (parsed_turn) begin
+			dial <= s_dial;
+			result <= s_result;
+		end
 	end
-
-	task turn_dial;
-		begin
-			case (direction)
-				L:
-					begin
-						dial = (dial - amount) % 100;
-						if (dial < 0)
-							dial = dial + 100;
-					end
-				R: dial = (dial + amount) % 100;
-			endcase
-
-			if (dial == 0)
-				result <= result + 1;
-		end
-	endtask
-
-	task reset_parser;
-		begin
-			direction <= L;
-			amount <= 0;
-		end
-	endtask
 endmodule
 
 module assignment6_part2 (
@@ -65,60 +39,91 @@ module assignment6_part2 (
 	input wire clk,
 	input wire rst
 );
-	localparam L = 0;
-	localparam R = 1;
+	wire parsed_turn;
+	wire signed [15:0] amount;
+	parser m_parser (parsed_turn, amount, character, enable_character, clk, rst);
 
 	reg signed [15:0] dial;
 
-	reg direction;
-	reg signed [15:0] amount;
+	reg signed [15:0] s_dial_signed;
+	reg [15:0] s_dial;
+	reg [15:0] s_result;
+	reg [15:0] s_amount_unsigned;
+	reg s_zero_hit_after_mod_100;
+
+	always @* begin
+		s_dial_signed = (dial + amount) % 100;
+		s_dial = s_dial_signed < 0 ? s_dial_signed + 100 : s_dial_signed;
+
+		s_amount_unsigned = amount < 0 ? -amount : amount;
+		s_zero_hit_after_mod_100 =
+			(amount < 0 && s_amount_unsigned % 100 >= dial && dial != 0) ||
+			(amount >= 0 && s_amount_unsigned % 100 + dial >= 100);
+
+		s_result = result + s_amount_unsigned / 100 + s_zero_hit_after_mod_100;
+	end
 
 	always @(posedge clk) begin
 		if (rst) begin
-			result <= 0;
 			dial <= 50;
-			reset_parser();
-		end else if (enable_character)
-			case (character)
-				"L": direction <= L;
-				"R": direction <= R;
-				"\n":
-					begin
-						turn_dial();
-						reset_parser();
-					end
-				default:
-					if (character >= "0" && character <= "9")
-						amount <= amount * 10 + character - "0";
-			endcase
+			result <= 0;
+		end else if (parsed_turn) begin
+			dial <= s_dial;
+			result <= s_result;
+		end
+	end
+endmodule
+
+module parser (
+	output wire parsed_turn,
+	output wire signed [15:0] amount,
+	input wire [7:0] character,
+	input wire enable_character,
+	input wire clk,
+	input wire rst
+);
+	localparam L = 0;
+	localparam R = 1;
+
+	assign parsed_turn = character == "\n" && enable_character;
+	assign amount = s_amount;
+
+	reg direction;
+	reg [15:0] abs_amount;
+
+	reg s_direction;
+	reg [15:0] s_abs_amount;
+	reg signed [15:0] s_amount;
+
+	always @* begin
+		case (character)
+			"L": s_direction = L;
+			"R": s_direction = R;
+			default: s_direction = direction;
+		endcase
+
+		case (character)
+			"L",
+			"R": s_abs_amount = abs_amount;
+			default:
+				if (character >= "0" && character <= "9")
+					s_abs_amount = abs_amount * 10 + character - "0";
+				else
+					s_abs_amount = 0;
+		endcase
+
+		case (direction)
+			L: s_amount = -abs_amount;
+			R: s_amount = abs_amount;
+		endcase
 	end
 
-	task turn_dial;
-		begin
-			if (
-				(direction == L && amount % 100 >= dial && dial != 0) ||
-				(direction == R && amount % 100 + dial >= 100)
-			)
-				result <= result + amount / 100 + 1;
-			else
-				result <= result + amount / 100;
-
-			case (direction)
-				L:
-					begin
-						dial = (dial - amount) % 100;
-						if (dial < 0)
-							dial = dial + 100;
-					end
-				R: dial = (dial + amount) % 100;
-			endcase
+	always @(posedge clk) begin
+		if (rst)
+			abs_amount <= 0;
+		else if (enable_character) begin
+			direction <= s_direction;
+			abs_amount <= s_abs_amount;
 		end
-	endtask
-
-	task reset_parser;
-		begin
-			direction <= L;
-			amount <= 0;
-		end
-	endtask
+	end
 endmodule
